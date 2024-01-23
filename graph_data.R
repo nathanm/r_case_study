14faf1d391d805b349fa0f88cfeecdc3eddf4e0b
Nathan Machula <nmachula@gmail.com>
2024-01-23 16:30:56 -0600

    Try to convert dailyIntensities to long data

--- graph_data.R
+++ graph_data.R
@@ -3,3 +3,6 @@ library(tidyverse)
 ggplot(dailyActivity, aes(x = date, y = Calories)) +
   geom_line() +
   geom_smooth()
+
+ggplot(dailyIntensities, aes(x = date, y = Calories, fill = )) +
+  geom_bar()
