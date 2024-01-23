fd0d3bcf0da2affd1988e9749c7747561d36843c
Nathan Machula <nathan.machula@co.buffalo.wi.us>
2024-01-23 16:30:56 -0600

    Try to convert dailyIntensities to long data

--- clean_data.R
+++ clean_data.R
@@ -3,3 +3,12 @@ library(lubridate)
 
 dailyActivity <- dailyActivity %>% 
   mutate(date = mdy(ActivityDate))
+
+dailyIntensity <- dailyIntensities %>% 
+  mutate(date = mdy(ActivityDay)) #%>% 
+  #select(-Id:ActivityDay) %>% 
+  pivot_longer(
+    cols = SedentaryMinutes:VeryActiveDistance,
+    #names_to = c("Sedentary", "LightlyActive", "FairlyActive", "VeryActive"),
+    names_to = c(".value","Minutes","Distance")
+  )
