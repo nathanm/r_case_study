be14bba7c32a9182600923d03bacbdffe81f363b
Nathan Machula <nmachula@gmail.com>
2024-01-24 10:00:24 -0600

    Transform dataframes & initial graphs

--- clean_data.R
+++ clean_data.R
@@ -2,13 +2,42 @@ library(tidyverse)
 library(lubridate)
 
 dailyActivity <- dailyActivity %>% 
-  mutate(date = mdy(ActivityDate))
+  mutate(date = mdy(ActivityDate)) %>% 
+  select(Id, date, TotalSteps:Calories)
 
-dailyIntensity <- dailyIntensities %>% 
-  mutate(date = mdy(ActivityDay)) %>% 
-  #select(-Id:ActivityDay) %>% 
+dailyCalories <- dailyCalories %>% 
+  mutate(
+    Id = as.character(Id),
+    date = mdy(ActivityDay),
+    Calories = as.integer(Calories)
+  ) %>% 
+  select(Id, date, Calories)
+
+Calories <- dailyCalories %>% 
+  group_by(Id) %>% 
+  summarize(average = mean(Calories)) %>% 
+  arrange(-average)
+
+dailyIntensities <- dailyIntensities %>% 
   pivot_longer(
-    cols = SedentaryMinutes:VeryActiveDistance,
-    #names_to = c("Sedentary", "LightlyActive", "FairlyActive", "VeryActive"),
-    names_to = c(".value","Minutes","Distance")
-  )
+    cols = !c(Id, ActivityDay),
+    names_to = c("intensity", "type"),
+    names_pattern = "(.*)(Minutes|Distance)"
+  ) %>% 
+  mutate(
+    date = mdy(ActivityDay),
+    intensity = factor(
+      intensity,
+      levels = c("Sedentary", "SedentaryActive", "LightlyActive", "LightActive", "FairlyActive", "ModeratelyActive", "VeryActive"),
+      labels = c("Sedentary", "Sedentary", "LightlyActive", "LightlyActive", "ModeratelyActive", "ModeratelyActive", "VeryActive")
+    )
+  ) %>% 
+  pivot_wider(
+    names_from = type,
+    values_from = value
+  ) %>% 
+  select(c(Id, date, intensity, Minutes, Distance))
+
+Intensity <- dailyIntensities %>% 
+  group_by(Id) %>% 
+  summarize(average = mean(Minutes))
--- graph_data.R
+++ graph_data.R
@@ -4,5 +4,11 @@ ggplot(dailyActivity, aes(x = date, y = Calories)) +
   geom_line() +
   geom_smooth()
 
+ggplot(dailyCalories, aes(x = date, y = Calories, color = Id)) +
+  geom_line()
+
+ggplot(Calories, aes(x = Id, y = average)) +
+  geom_col()
+
 ggplot(dailyIntensities, aes(x = date, y = Calories, fill = )) +
   geom_bar()
